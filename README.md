15525dabadf97e6fd84fad7215b02cc597d86dc3
PWCRAFT <163775200+PWCRAFT@users.noreply.github.com>
2024-10-19 19:56:48 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# MOVIEDATABASE-PROJECT-
-This is a movie Database application using HTML, CSS, JavaScript, and React (with optional Tailwind CSS). The application will allow users to search for movies and view detailed information about them, using a public movie API.
+![oomdb](https://github.com/IsraelBosun/omdbapi/assets/110021253/46e7c5b0-bf6b-4ef8-8bd8-49409b96c1c6)
+
+ 
+ <h1>About Project</h1>
+
+<p>This project is a movie search application built with Vite and React. It utilizes the OMDB API to access movie details and displays them on the screen.</p>
+
+<h2>Installation</h2>
+
+<ol>
+  <li>Clone the repository:</li>
+</ol>
+<pre><code class="language-shell">git clone https://github.com/IsraelBosun/omdbapi.git</code></pre>
+
+<ol start="2">
+  <li>Navigate to the project directory:</li>
+</ol>
+<pre><code class="language-shell">cd omdbapi</code></pre>
+
+<ol start="3">
+  <li>Install dependencies:</li>
+</ol>
+<pre><code class="language-shell">npm install</code></pre>
+
+<h2>Usage</h2>
+
+<ol>
+  <li>Obtain an API key from the OMDB API website: <a href="https://www.omdbapi.com/">OMDB API</a></li>
+  <li>Create a <code>.env</code> file at the root of the project and add the following line:</li>
+</ol>
+<pre><code>VITE_OMDB_API_KEY=your_api_key_here</code></pre>
+
+<ol start="3">
+  <li>Start the development server:</li>
+</ol>
+<pre><code class="language-shell">npm run dev</code></pre>
+
+<ol start="4">
+  <li>Open your browser and visit <a href="http://localhost:3000">http://localhost:3000</a> to view the application.</li>
+  <li>Enter a movie title in the search bar to fetch movie details from the OMDB API.</li>
+</ol>
+
+<h2>Built With</h2>
+
+<ul>
+  <li>Vite - Fast development build tool</li>
+  <li>React - JavaScript library for building user interfaces</li>
+  <li>OMDB API - Open Movie Database API for accessing movie details</li>
+ <li>Tailwind CSS - A CSS framework I find personally efficient at coding </li>
+</ul>
+
+<h2>View the deployed app here</h2>
+
+<p>This app was deployed using Vercel, click <a href="https://movieapp-one-eta.vercel.app/">here</a></p>
+
---
+++ src/Home.jsx
@@ -0,0 +1,116 @@
+import React, { useEffect, useState } from 'react';
+import UseFetch from './UseFetch';
+import Head from './Head';
+import { ImSpinner2 } from 'react-icons/im';
+
+const Home = () => {
+  const [search, setSearch] = useState('');
+  const [filtered, setFiltered] = useState([]);
+  const [selectedMovie, setSelectedMovie] = useState(null);
+
+
+  // console.log(import.meta.env.VITE_OMDB_API_KEY);
+  // console.log("Hello")
+
+
+
+  const searchApi = `https://www.omdbapi.com/?s=${encodeURIComponent(search)}&apiKey=${import.meta.env.VITE_OMDB_API_KEY}`;
+  const detailApi = `https://www.omdbapi.com/?i=`;
+  // console.log("This is it", import.meta.env.VITE_OMDB_API_KEY);
+
+  const { loading, error, data } = UseFetch(searchApi);
+  
+  const movies = data;
+  console.log(movies)
+  
+  useEffect(() => {
+    const debounce = setTimeout(() => {
+      const fetchMovieDetails = async () => {
+        if (movies && movies.Search) {
+          const filteredMovies = [];
+          
+          for (const movie of movies.Search) {
+            const response = await fetch(detailApi + movie.imdbID + `&apikey=${import.meta.env.VITE_OMDB_API_KEY}`);
+            
+            const data = await response.json();
+            filteredMovies.push(data);
+          }
+          
+          setFiltered(filteredMovies);
+        }
+      };
+      
+      fetchMovieDetails();
+    }, 350);
+    
+    return () => {
+      clearTimeout(debounce);
+    };
+  }, [movies, detailApi]);
+  
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+  
+  const handleClick = (movie) => {
+    setSelectedMovie(movie);
+  };
+  
+  const handleClose = () => {
+    setSelectedMovie(null);
+  };
+  
+  return (
+    <div className="px-9  text-white ">
+      <Head />
+
+      <div className="flex my-9 pb-[3rem] items-center w-full justify-center">
+        <input type="text" className="search_input  text-black max-w-[900px]" value={search} onChange={handleChange} />
+      </div>
+
+      {loading ? (
+        <ImSpinner2 className="mx-auto animate-spin text-violet-700 text-4xl mt-[200px]" />
+      ) : error ? (
+        <div className="mx-auto  text-violet-700 text-3xl mt-[200px]">Movie Not Found</div>
+      ) : (
+        <div className="mt-9 grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-12">
+          {filtered.map((movie) => (
+            <div key={movie.imdbID} className="flex flex-col max-w-[300px]">
+              <div className="">
+                <img src={movie.Poster} alt="" className="rounded-xl" />
+              </div>
+              <div className="flex flex-col gap-4">
+                <div className="flex gap-4 mt-3 items-center justify-between">
+                  <p>{movie.Title}</p>
+                  <p>{movie.Released}</p>
+                </div>
+                <button className="bg-blue-600 p-2 rounded-lg text-xl hover:bg-white hover:text-blue-600 " onClick={() => handleClick(movie)}>
+                  view more
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+
+      {selectedMovie && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-70 transition duration-500">
+          <div className="bg-white text-black p-6 max-w-md rounded-lg">
+            <h2 className="text-2xl font-bold mb-4">{selectedMovie.Title}</h2>
+            <p><span className='bold'>Director:</span> {selectedMovie.Director}</p>
+            <p><span className='bold'>Genre:</span> {selectedMovie.Genre}</p>
+            <p><span className='bold'>Plot:</span> {selectedMovie.Plot}</p>
+            <p><span className='bold'>Rating:</span> {selectedMovie.imdbRating}/10</p>
+            <button className="bg-blue-600 p-2 rounded-lg text-white mt-4" onClick={handleClose}>
+              Close
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Home;
+
+
